a34027a1197b9975df07c8946d12ab8eae6e0971
Edward Ang <wei.hen.ang@gmail.com>
2024-08-31 15:45:00 +0800

    Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import TopHeader from '@/components/Header/header';
 import BackToTop from '@/components/BackToTop/backToTop';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className='home-div' style={{height: '500vh'}}>
+      <TopHeader />
       <BackToTop />
     </div>
   );
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,38 @@
 'use client';
+import { Input } from 'antd';
+import Link from 'next/link';
+import Image from 'next/image';
+import './header.css';
+
+const { Search } = Input;
+
+const onSearch = (value) => console.log(value);
 
 const TopHeader = () => {
   return (
-    <div>
-        
-    </div>
+    <header>
+      <div className="header-container">
+        <Link className='header-logo' href='/'>
+          <Image
+            className='logo'
+            src='/favicon.ico'
+            alt='logo'
+            width={32} // Adjust the width as needed
+            height={32} // Adjust the height as needed
+          />
+          <h2 className='name'>PopWatch</h2>
+        </Link>
+        <Search
+          className='custom-search'
+          placeholder="input search text"
+          onSearch={onSearch}
+          enterButton 
+          style={{
+            width: 300,
+          }}/>
+      </div>
+    </header>
   );
 };
+
 export default TopHeader;
